76e89015d4da9b6fb6e64d62cc49858ab7c3b3eb
DonnoWanderlley <donnovanwanderlleycandelasramo@gmail.com>
2026-02-01 19:14:40 -0600

    Nodes and Coords added

--- src/Grafos.jsx
+++ src/Grafos.jsx
@@ -1,11 +1,13 @@
-// import { useState } from "react";
+import { coords } from "./data";
+import Node from "./components/Node";
 
 // ===================== Grafos =====================
 export default function Grafos() {
-
     return (
         <div className="container">
-            
+            {Object.keys(coords).map((node) => (
+                <Node key={node} id={node} coords={coords} />
+            ))}
         </div>
     );
 }
---
+++ src/components/Node.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Node({ id, coords }) {
+    return (
+        <div
+        style={{
+            position: "absolute",
+            left: coords[id].x,
+            top: coords[id].y,
+            width: 20,
+            height: 20,
+            borderRadius: "50%",
+            background: "#003e81",
+            color: "white",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            cursor: "pointer",
+            fontSize: 9,
+            opacity: 1
+        }}
+        >
+        {id}
+        </div>
+    );
+}
